86376593263c4863138e754ec929d3a9eb79256c
Hamisi Rawlins <53607315+hamisirawlins@users.noreply.github.com>
2021-04-18 18:58:31 +0300

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 const App = () => {
     return(
-        <div>
-<h1>Offerspot</h1>
-        </div>
+       <Container maxwidth="lg">
+           <AppBar position="static" color="inherit">
+               <Typography variant="h2" align="center">Offerspot</Typography>
+               <img src={} alt="offers" height="60"/>
+           </AppBar>
+
+       </Container>
     ) 
 }
 
