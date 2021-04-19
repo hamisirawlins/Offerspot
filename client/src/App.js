495a2b9c5a2ad4d4827c0d60a4a146c0c94867f6
Hamisi Rawlins <53607315+hamisirawlins@users.noreply.github.com>
2021-04-19 20:14:59 +0300

    Client setup, styles and redux directories

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
     return(
        <Container maxwidth="lg">
-           <AppBar position="static" color="inherit">
-               <Typography variant="h2" align="center">Offerspot</Typography>
+           <AppBar className={classes.appBar} position="static" color="inherit">
+               <Typography className={classes.heading} variant="h2" align="center">Offerspot</Typography>
            </AppBar>
+           <Grow in>
+               <Container>
+                   <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                       <Grid item xs={12} sm={7}>
+                           <Posts/>
 
+                       </Grid>
+                       <Grid item xs={12} sm={4}>
+                           <Form/>
+
+                       </Grid>
+
+                   </Grid>
+        
+               </Container>
+           </Grow>
        </Container>
     ) 
 }
