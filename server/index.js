8c9d73853c3bd1b1090fc9cf40191097327eacfe
Hamisi Rawlins <53607315+hamisirawlins@users.noreply.github.com>
2021-05-05 11:17:27 +0300

    Heroku backend deployment

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/',(req,res)=>{
+    res.send('The Offerspot Api is up');
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
